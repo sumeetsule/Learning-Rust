8a173ce353857b4a30df60191dbd4e3fb0a709d0
Sumeet_Sule <sule.sumeet@gmail.com>
2024-05-17 00:18:45 +0530

    assert macros!

---
+++ assert_macros.rs
@@ -0,0 +1,20 @@
+// Different assert macros
+fn main() {
+let result = add(2,2);
+
+assert!(result == 4);
+assert!(result == 4,"Expected 4; returned result is {}",result);
+
+
+
+// let result = add(2,2);
+
+// assert_eq!(result,4);
+
+// assert_eq!(result,4,"Expected 4; returned result is {}", result);
+
+
+
+// //Similar to the previous one, checks for NOT Equalto.
+// assert_ne!()
+}
